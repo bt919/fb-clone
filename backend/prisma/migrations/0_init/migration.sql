c53bf6113d99bb44ac3fd60292b7b25dfdb0ed50
Hisoka <gintama2009@hotmail.ca>
2024-10-03 22:08:11 +0000

    chore: set up prisma and postgres

---
+++ backend/prisma/migrations/0_init/migration.sql
@@ -0,0 +1,16 @@
+-- CreateEnum
+CREATE TYPE "gender_type" AS ENUM ('male', 'female', 'other');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "public_id" TEXT NOT NULL,
+    "email" VARCHAR(320) NOT NULL,
+    "hashed_password" TEXT NOT NULL,
+    "first_name" VARCHAR(50) NOT NULL,
+    "last_name" VARCHAR(50) NOT NULL,
+    "gender" "gender_type" NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
---
+++ backend/sql/ddl.sql
@@ -0,0 +1,11 @@
+CREATE TYPE gender_type AS ENUM('male', 'female', 'other');
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    public_id TEXT NOT NULL, -- short uuid
+    email VARCHAR(320) NOT NULL,
+    hashed_password TEXT NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    gender gender_type NOT NULL
+);
