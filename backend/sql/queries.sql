bec2344db6c088d67841e0c915d3a070e5223c39
Hisoka <gintama2009@hotmail.ca>
2024-11-23 14:00:26 +0000

    chore: add db queries to retrieve a user and their friends' posts

--- backend/sql/queries.sql
+++ backend/sql/queries.sql
@@ -132,15 +132,34 @@ SELECT u.public_id,
         COUNT(DISTINCT r.id) AS number_of_likes,
         COUNT(DISTINCT c.id) AS number_of_comments
 FROM users u 
-JOIN posts p ON u.id = p.author_id
+JOIN posts p ON u.id = p.author_id AND u.public_id = $1
 LEFT OUTER JOIN reactions r ON p.id = r.post_id
 LEFT OUTER JOIN comments c ON p.id = c.post_id
 WHERE u.public_id = $1
 GROUP BY p.id, u.id
-ORDER BY p.posted_at;
+ORDER BY p.posted_at DESC
+LIMIT $2
+OFFSET $3;
 
 -- retrieve your posts as well as your friends' posts (for home page)
-
+SELECT u1.public_id,
+        u1.first_name || ' ' || u1.last_name AS full_name,
+        u1.profile_image_key,
+        p.id AS post_id,
+        p.posted_at,
+        COUNT(r.user_id = u.id) > 0 AS liked_by_user,
+        COUNT(DISTINCT r.id) AS number_of_likes,
+        COUNT(DISTINCT c.id) AS number_of_comments
+FROM users u
+JOIN friends_with f ON u.id = f.user_one_id AND u.public_id = $1
+JOIN posts p ON u.id = p.author_id OR f.user_two_id = p.author_id
+JOIN users u1 ON u1.id = p.author_id
+LEFT OUTER JOIN reactions r ON p.id = r.post_id
+LEFT OUTER JOIN comments c ON p.id = c.post_id
+GROUP BY p.id, u.id, u1.id
+ORDER BY p.posted_at DESC
+LIMIT $2
+OFFSET $3;
 
 -- 7jfMSPx2PhLj17nLdgxnLW
 
